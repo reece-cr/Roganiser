e267a6329d2ee9338d6f18cd857d7b95afc78811
reece crane <rcrane948@hotmail.co.uk>
2024-09-13 11:59:17 +0100

    made side panel

---
+++ Roganiser/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roganiser.Models
+{
+    class Task
+    {
+        Guid Id { get; set; }
+        string Title { get; set; } = string.Empty;
+        string Description {  get; set; } = string.Empty;
+        bool IsCompleted { get; set; }
+
+    }
+}
